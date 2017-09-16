9d2b7f3d35a8fe97628de295f756eaaf90448a0d
user <user@PATA16.local>
2017-09-16 11:11:17 +0700

    Initial Commit

---
+++ MycontactList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MycontactList
+//
+//  Created by user on 9/16/2560 BE.
+//  Copyright © 2560 asdfghjkl;'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
